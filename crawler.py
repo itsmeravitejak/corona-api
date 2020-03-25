9a6bbe19512c863ad2b236b35330f111c80c6f22
ravitejak <ravitejak@live.in>
2020-03-25 15:04:38 +0530

    updates to crawler

--- crawler.py
+++ crawler.py
@@ -8,37 +8,38 @@ urls=   [
 requests = (grequests.get(u) for u in urls)
 responses = grequests.map(requests)
 
-labels=["Screened","Active","Discharged","Migrated","Deaths"]
+labels=["Screened","Active","Discharged","Deaths","Migrated"]
 
 overall_list=[]
 statewide_list=[]
 
 for i, response in enumerate(responses):
     soup = BeautifulSoup(response.text, 'html.parser')
-    selectedtags = soup.select("tbody tr")
+    selectedtags = soup.select("#cases tbody tr")
     
     for row in selectedtags:
         tds = row.select("td")
+        print(tds)
         if(tds[0].text.lower().find("India".lower()) != -1):
-            statewide_list.append({"Name":"Total","Confirmed-Indian":tds[1].text,"Confirmed-Foreign":int(tds[2].text.strip('\r\n')),"Cured/Discharged/Migrated":int(tds[3].text.strip('\r\n')),"Death":int(tds[4].text.strip('\r\n'))})
+            statewide_list.append({"Name":"Total","Confirmed-Indian":tds[1].text,"Confirmed-Foreign":int(tds[2].text.strip('\r\n#*')),"Cured/Discharged/Migrated":int(tds[3].text.strip('\r\n#*')),"Death":int(tds[4].text.strip('\r\n#*'))})
         else:
-            statewide_list.append({"Name":tds[1].text.strip('\r\n'),"Confirmed-Indian":int(tds[2].text),"Confirmed-Foreign":int(tds[3].text),"Cured/Discharged/Migrated":int(tds[4].text),"Death":int(tds[5].text)})
+            statewide_list.append({"Name":tds[1].text.strip('\r\n#*'),"Confirmed-Indian":int(tds[2].text.strip('\r\n#*')),"Confirmed-Foreign":int(tds[3].text.strip('\r\n#*')),"Cured/Discharged/Migrated":int(tds[4].text.strip('\r\n#*')),"Death":int(tds[5].text.strip('\r\n#*'))})
+    
+
+    selectedtags =  soup.select("span.icount")
+    count=0
+    for singletag in selectedtags:          
+        txt=singletag.getText()
+        if(count<len(labels)):
+            overall_list.append({"label":labels[count],"value":int(txt.strip().replace(",","").replace("#",""))})
+        count+=1
         
-    selectedtags =  soup.select("ol strong")
-    for singletag in selectedtags:        
-        txt=singletag.getText().split(":")
-        if(len(txt)>1):
-            # print(txt[0])
-            # print(int(txt[1].strip().replace(",","")))
-            for label in labels:
-                if(txt[0].lower().find(label.lower()) !=-1):
-                    overall_list.append({"label":label,"Description":txt[0].strip("\t"),"value":int(txt[1].strip().replace(",",""))})
                         
 import pprint
 pprint.pprint(statewide_list)
-with open('api/statewide.json', 'w') as file:
-    file.write(json.dumps(statewide_list))
-    print "Saved Statewide data"
+# with open('api/statewide.json', 'w') as file:
+#     file.write(json.dumps(statewide_list))
+#     print "Saved Statewide data"
 
 pprint.pprint(overall_list)
 with open('api/overall.json', 'w') as file:
